90d5859e00a2dc0c50342a243cce10cd1fc44fd1
222twotwotwo <3379571673@qq.com>
2026-02-14 00:11:25 +0800

    优化数据库，删除冗余

--- backend/databaseinit/migrate_image_path.sql
+++
@@ -1,5 +0,0 @@
--- 为文档表增加图片路径字段，用于“拖拽上传图片”功能：删除文档时同步删除服务器上的图片文件
--- 使用方式：在 MySQL 中执行本文件，例如：mysql -u root -p markdown_editor < databaseinit/migrate_image_path.sql
-USE markdown_editor;
-
-ALTER TABLE documents ADD COLUMN image_path VARCHAR(500) NULL DEFAULT NULL COMMENT '图片文档对应的服务器相对路径，如 images/xxx.png';
